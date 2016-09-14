c0cc880f9043d38a4305d1eedc4e0873f8ff6df8
ekhaydarov <erkin.khaydarov@holidayextras.com>
2016-09-14 14:16:46 +0100

    pepe edits

--- check_circle_status.py
+++ check_circle_status.py
@@ -3,9 +3,9 @@ import sys
 import time
 
 # Pass in circleci environment variable from a pipeline repo
-github_organisation = sys.argv[1] # GITHUB_ORG
-repository = sys.argv[2] # NEXT_PROJECT
-circle_token = sys.argv[3] # CIRCLE_TOKEN
+github_organisation = sys.argv[1]  # GITHUB_ORG
+repository = sys.argv[2]  # NEXT_PROJECT
+circle_token = sys.argv[3]  # CIRCLE_TOKEN
 response_limit = '1'
 
 
@@ -13,7 +13,8 @@ def circle_request():
     json_attempts = 10
 
     while json_attempts > 0:
-        r = requests.get('https://circleci.com/api/v1.1/project/github/{}/{}/tree/dependency-graph?circle-token={}&limit={}'.format(
+        r = requests.get('https://circleci.com/api/v1.1/project/github/'
+            '{}/{}/tree/dependency-graph?circle-token={}&limit={}'.format(
             github_organisation,
             repository,
             circle_token,
@@ -39,7 +40,7 @@ def circle_status():
             return 'kicking off pipeline controller'
 
     if poll_tries == 0:
-        # TODO consider writing to stderr for easier to spot red content in circleci
+        # TODO write to stderr easier to spot red content in circleci
         print('Ran out of time!')
         exit(1)
 
