b15c05c4e6a987f23f37433714fc94b236b5da1a
ekhaydarov <erkin.khaydarov@holidayextras.com>
2016-09-14 16:16:09 +0100

    editing setup.py to make it work as a dependency

--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ setup(
 
     # You can just specify the packages manually here if your project is
     # simple. Or you can use find_packages().
-    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+    # packages=find_packages(exclude=['contrib', 'docs', 'tests']),
 
     # Alternatively, if you want to distribute just a my_module.py, uncomment
     # this:
@@ -63,7 +63,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['requests', 'mox'],
+    # install_requires=['requests', 'mox'],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
